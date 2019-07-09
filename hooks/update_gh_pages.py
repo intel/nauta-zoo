ac5bb44ba1a30fffc238d76beb41ac722eec38a4
Mateusz Ciesielski <mateusz.j.ciesielski@intel.com>
2019-07-09 14:02:03 +0200

    CAN-2193 further improvements, tests

--- hooks/update_gh_pages.py
+++ hooks/update_gh_pages.py
@@ -1,5 +1,6 @@
-#!/bin/python3
-# Pre commit hook script
+#!/usr/bin/env python3
+
+# This script may be used as pre commit hook
 
 import json
 import subprocess
@@ -29,7 +30,8 @@ def get_template_dirs(repository_filenames: Sequence[str]) -> Set[str]:
 
 def pack_template_dirs(repo_path: str, template_dirs: Set[str]):
     for template_dir in template_dirs:
-        subprocess.check_call(['tar', '-czf', f'{repo_path}/docs/{template_dir}.tar.gz', f'{repo_path}/{template_dir}'])
+        subprocess.check_call(['tar', '-czf', f'{repo_path}/docs/{template_dir}.tar.gz', '-C',
+                               f'{repo_path}', f'{template_dir}'])
 
 # TODO: Calculate packs checksums
 
@@ -43,8 +45,8 @@ def parse_template_metadata(chart_yaml_path: str, metadata_fields: Set[str]) -> 
                     template_metadata[metadata_field] = line.split(':')[-1].strip()
 
     for metadata_field in metadata_fields:
-    if not template_metadata.get(metadata_field):
-        raise ValueError(f'Failed to get {metadata_field} field from {chart_yaml_path}')
+        if not template_metadata.get(metadata_field):
+            raise ValueError(f'Failed to get {metadata_field} field from {chart_yaml_path}')
 
     return template_metadata
 
@@ -71,17 +73,16 @@ def prepare_templates_json(repo_path: str, template_dirs: Set[str], templates_me
                        'url': f'{template_dir}.tar.gz',
                        'version': templates_metadata[template_dir]['version'],
                        'description': templates_metadata[template_dir]['description']}
-                      for template_dir in template_dirs],
+                      for template_dir in sorted(template_dirs)],
     }
     with open(f'{repo_path}/docs/index.json', mode='w+', encoding='utf-8') as manifest_file:
         json.dump(zoo_manifest, manifest_file, indent=2)
 
 
-def main(argv=None):
+def main():
     repo_path = get_repo_path()
     filenames = get_repository_filenames(repo_path)
     template_dirs = get_template_dirs(filenames)
-    print(template_dirs)
     pack_template_dirs(repo_path, template_dirs)
     templates_metadata = get_templates_metadata(repo_path, template_dirs)
     prepare_templates_json(repo_path, template_dirs, templates_metadata)
